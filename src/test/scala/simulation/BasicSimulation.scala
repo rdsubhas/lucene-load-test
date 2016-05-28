e37ada9064bf45715130786e36c40fa304c8c9f2
Subhas Dandapani <rdsubhas@gmail.com>
2016-05-28 02:40:01 +0200

    Add multiple readers to simulate services

--- src/test/scala/simulation/BasicSimulation.scala
+++ src/test/scala/simulation/BasicSimulation.scala
@@ -18,40 +18,47 @@ import scala.concurrent.duration._
 class BasicSimulation extends Simulation {
   val noop = new SessionHookBuilder(session => session)
 
-  val scn = scenario("lookup").
-    exec(noop).
-    feed(csv("ids.csv").circular).
-    exec(new LookupActionBuilder)
+  val scenarios = (1 to 10).toList.map(n => {
+    scenario("lookup" + n)
+      .exec(noop)
+      .feed(csv("ids.csv").circular)
+      .exec(new LookupActionBuilder)
+      .inject(constantUsersPerSec(100) during 1.minutes)
+  })
 
-  setUp(scn.inject(constantUsersPerSec(100) during(1 minutes)))
+  setUp(scenarios)
 
 }
 
-object LookupAction {
-  val reader = DirectoryReader.open(FSDirectory.open(PathHelper.luceneRoot))
-  val searcher = new IndexSearcher(reader)
-}
-
-class LookupAction(val statsEngine: StatsEngine, val next: Action)
+class LookupAction(val reader: DirectoryReader, val searcher: IndexSearcher, val statsEngine: StatsEngine, val next: Action)
   extends ChainableAction with NameGen with ExitableAction {
-
   override def name: String = "lucene-lookup"
+
+  val found = Some("404")
+  val not_found = Some("200")
 
   override def execute(session: Session): Unit = {
     val startTime = System.currentTimeMillis
     val doc_id = session("doc_id").as[String]
     val query = new TermQuery(new Term("id", doc_id))
-    val hits = LookupAction.searcher.search(query, 1)
-    val hit = hits.scoreDocs(0)
-    val doc = LookupAction.reader.document(hit.doc)
-    val endTime = System.currentTimeMillis
-    statsEngine.logResponse(session, name, new ResponseTimings(startTime, endTime), OK, None, None)
+    val hits = searcher.search(query, 1)
+    if (hits.totalHits > 0) {
+      val hit = hits.scoreDocs(0)
+      val doc = reader.document(hit.doc)
+      val endTime = System.currentTimeMillis
+      statsEngine.logResponse(session, name, new ResponseTimings(startTime, endTime), OK, found, None)
+    } else {
+      val endTime = System.currentTimeMillis
+      statsEngine.logResponse(session, name, new ResponseTimings(startTime, endTime), OK, not_found, None)
+    }
     next ! session
   }
 }
 
 class LookupActionBuilder extends ActionBuilder {
   override def build(ctx: ScenarioContext, next: Action): Action = {
-    new LookupAction(ctx.coreComponents.statsEngine, next)
+    val reader = DirectoryReader.open(FSDirectory.open(PathHelper.luceneRoot))
+    val searcher = new IndexSearcher(reader)
+    new LookupAction(reader, searcher, ctx.coreComponents.statsEngine, next)
   }
 }
