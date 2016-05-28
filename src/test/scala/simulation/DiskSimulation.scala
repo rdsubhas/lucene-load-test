fb09c07dc205643fe185ca15a7cd5b0d61cdb5ba
Subhas Dandapani <rdsubhas@gmail.com>
2016-05-28 22:48:24 +0200

    Randomize document ids to simulate random access

--- src/test/scala/simulation/DiskSimulation.scala
+++ src/test/scala/simulation/DiskSimulation.scala
@@ -1,5 +1,7 @@
 package simulation
 
+import java.lang.Integer.parseInt
+
 import gatling.PathHelper
 import io.gatling.commons.stats.OK
 import io.gatling.core.Predef._
@@ -15,15 +17,21 @@ import org.apache.lucene.store.FSDirectory
 
 import scala.concurrent.duration._
 
-class BasicSimulation extends Simulation {
+class DiskSimulation extends Simulation {
   val noop = new SessionHookBuilder(session => session)
+  val threads = parseInt(System.getProperty("test.threads", "10"))
+  val users = parseInt(System.getProperty("test.users", "1000"))
+  val duration = parseInt(System.getProperty("test.duration", "1")).minutes
 
-  val scenarios = (1 to 10).toList.map(n => {
-    scenario("lookup" + n)
+  val scenarios = (1 to threads).toList.map(n => {
+    scenario(s"lucene-disk-${n}")
       .exec(noop)
-      .feed(csv("ids.csv").circular)
+      .feed(csv("part1.csv").random.circular)
+      .feed(csv("part2.csv").random.circular)
+      .feed(csv("part3.csv").random.circular)
+      .feed(csv("part4.csv").random.circular)
       .exec(new LookupActionBuilder)
-      .inject(constantUsersPerSec(100) during 1.minutes)
+      .inject(constantUsersPerSec(users) during(duration))
   })
 
   setUp(scenarios)
@@ -32,25 +40,37 @@ class BasicSimulation extends Simulation {
 
 class LookupAction(val reader: DirectoryReader, val searcher: IndexSearcher, val statsEngine: StatsEngine, val next: Action)
   extends ChainableAction with NameGen with ExitableAction {
-  override def name: String = "lucene-lookup"
+  override def name: String = "disk"
 
-  val found = Some("404")
-  val not_found = Some("200")
+  val hit_name = "disk-hit"
+  val hit_code = Some("404")
+  val miss_name = "disk-miss"
+  val miss_code = Some("200")
+  val paths = List("part1", "part2", "part3", "part4")
 
   override def execute(session: Session): Unit = {
     val startTime = System.currentTimeMillis
-    val doc_id = session("doc_id").as[String]
+    val doc_id = paths.map(p => session(p).as[String]).dropWhile(_.isEmpty).mkString("/")
     val query = new TermQuery(new Term("id", doc_id))
     val hits = searcher.search(query, 1)
-    if (hits.totalHits > 0) {
+    val hasHit = hits.totalHits > 0
+
+    if (hasHit) {
       val hit = hits.scoreDocs(0)
       val doc = reader.document(hit.doc)
-      val endTime = System.currentTimeMillis
-      statsEngine.logResponse(session, name, new ResponseTimings(startTime, endTime), OK, found, None)
-    } else {
-      val endTime = System.currentTimeMillis
-      statsEngine.logResponse(session, name, new ResponseTimings(startTime, endTime), OK, not_found, None)
     }
+
+    val endTime = System.currentTimeMillis
+    statsEngine.logResponse(
+      session,
+      if (hasHit) hit_name else miss_name,
+      new ResponseTimings(startTime, endTime),
+      OK,
+      if (hasHit) hit_code else miss_code,
+      None,
+      List(doc_id)
+    )
+
     next ! session
   }
 }
